aced3ba848feb8a654183cafd7bf8d9878951358
Sabin Adams <sabintheworld@gmail.com>
2022-08-09 14:03:16 -0700

    First commit

---
+++ src/components/UserDisplay.tsx
@@ -0,0 +1,23 @@
+import type { User } from '../types'
+import MessageDisplay from './MessageDisplay'
+
+type Props = {
+    user: User
+}
+
+function UserDisplay({ user }: Props) {
+    return <div className="flex gap-x-24 justify-center">
+        <div className="rounded-sm flex justify-center items-center drop-shadow-md bg-neutral-700 w-48 h-20">
+            <p className="text-xl text-gray-200 font-bold">
+                {user.name}
+            </p>
+        </div>
+        <div>
+            {
+                user.messages.map((message, index) => <MessageDisplay message={message} index={index} key={index}/>)
+            }
+        </div>
+    </div>
+}
+
+export default UserDisplay
